dd254b607f8b049581620f80823e2f6aec1ec75c
mf220061 <mf220061@g.u-fukui.ac.jp>
2022-07-01 14:14:12 +0000

    add web.

--- app/app.py
+++ app/app.py
@@ -2,3 +2,33 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
+ 
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'roytuts'
+app.config['MYSQL_DATABASE_HOST'] = 'db'
+mysql.init_app(app)
+
+import pymysql
+from flask import jsonify
+
+@app.route('/')
+def users():
+    conn = mysql.connect()
+    
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    cursor.execute("SELECT * FROM user")
+    
+    rows = cursor.fetchall()
+    
+    resp = jsonify(rows)
+    resp.status_code = 200
+    
+    return resp
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0')
