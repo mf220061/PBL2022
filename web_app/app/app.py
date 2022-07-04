a4b5a78bcf8dc683de551ed5239c208d822dade4
mf220061 <mf220061@g.u-fukui.ac.jp>
2022-07-04 17:38:59 +0900

    add comment.

--- web_app/app/app.py
+++ web_app/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, current_app
 
 app = Flask(__name__)
 
@@ -7,10 +7,19 @@ from flaskext.mysql import MySQL
 mysql = MySQL()
  
 # MySQL configurations
+"""
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_DB'] = 'roytuts'
 app.config['MYSQL_DATABASE_HOST'] = 'db'
+"""
+app.config.from_mapping(
+    MYSQL_DATABASE_USER='root',
+    MYSQL_DATABASE_PASSWORD='root',
+    MYSQL_DATABASE_DB='roytuts',
+    MYSQL_DATABASE_HOST='db',
+)
+# mysql.init_app(app)でapp.configで設定したflaskの設定値をmysqlに渡す準備をしている感じ
 mysql.init_app(app)
 
 import pymysql
@@ -22,13 +31,15 @@ def users():
     
     cursor = conn.cursor(pymysql.cursors.DictCursor)
     cursor.execute("SELECT * FROM user")
-    
+
     rows = cursor.fetchall()
-    
+
     resp = jsonify(rows)
     resp.status_code = 200
     
     return resp
 
+"""
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
+"""
--- web_app/start.sh
+++ web_app/start.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-docker container rm $(docker container ls -a -q)
-docker image rm $(docker image ls -a -q)
+docker container rm $(docker container ls -a -f status=exited -q)
+docker container rm -f pbl-app
+docker container rm -f pbl-web
+docker container rm -f pbl-adminer
+docker container rm -f pbl-db
+docker image rm $(docker image ls -a -f dangling=true -q)
+docker image rm web_app_app
 
 docker compose up
