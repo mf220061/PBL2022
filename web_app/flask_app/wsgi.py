dba43d5639dbca9a2d3dbf5997818557ad464ca6
mf220061 <mf220061@g.u-fukui.ac.jp>
2022-07-05 11:04:28 +0000

    change mysql connector.

--- web_app/flask_app/wsgi.py
+++ web_app/flask_app/wsgi.py
@@ -1,4 +1,47 @@
-from app import app
+from flask import jsonify, Flask
+import os
+import mysql.connector as mysql
+
+app = Flask(__name__)
+
+app.config.from_mapping(
+    SECRET_KEY='dev',
+)
+
+MYSQL_DATABASE_USER='root'
+MYSQL_DATABASE_PASSWORD='root'
+MYSQL_DATABASE_DB='blog'
+MYSQL_DATABASE_HOST='db'
+
+@app.route('/')
+def index():
+    return 'Root'
+
+@app.route('/dev')
+def users():
+    connect = mysql.connect(
+            user=MYSQL_DATABASE_USER,
+            database=MYSQL_DATABASE_DB,
+            password=MYSQL_DATABASE_PASSWORD,
+            host=MYSQL_DATABASE_HOST,
+            #port=MYSQL_DATABASE_PORT,
+        )
+    
+    cursor = connect.cursor()
+    cursor.execute("SELECT * FROM user")
+
+    rows = cursor.fetchall()
+
+    cursor.close()
+
+    resp = jsonify(rows)
+    
+    return resp
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
+
 
 if __name__ == "__main__":
     app.run()
