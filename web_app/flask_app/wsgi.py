5341b2f036e934b098a42bb37114b3953d7f3bb2
mf220061 <mf220061@g.u-fukui.ac.jp>
2022-07-27 13:17:25 +0000

    modify.

--- web_app/flask_app/wsgi.py
+++ web_app/flask_app/wsgi.py
@@ -1,6 +1,7 @@
 from flask import jsonify, Flask
 import os
 import mysql.connector as mysql
+from flask_bootstrap import Bootstrap
 
 def create_app(test_config=None):
     # create and configure the app
@@ -10,6 +11,8 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'test.db'),
     )
+
+    bootstrap = Bootstrap(app)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
