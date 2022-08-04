6e081fbe696559c29a08092e805b3342a468a938
mf220061 <mf220061@g.u-fukui.ac.jp>
2022-08-04 08:26:00 +0000

    compiler

--- web_app/db/schema.sql
+++ web_app/db/schema.sql
@@ -6,24 +6,30 @@ DROP TABLE IF EXISTS post;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  username varchar(65535) UNIQUE NOT NULL,
-  password varchar(65535) NOT NULL
+  username varchar(1024) CHARACTER SET ascii UNIQUE NOT NULL,
+  password varchar(1024) NOT NULL
 );
 
 CREATE TABLE post (
   id INTEGER PRIMARY KEY AUTO_INCREMENT,
   author_id INTEGER NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  title varchar(65535) NOT NULL,
-  body varchar(65535) NOT NULL,
+  title varchar(1024) NOT NULL,
+  body varchar(1024) NOT NULL,
   FOREIGN KEY (author_id) REFERENCES user (id)
 );
 
-CREATE TABLE `test` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `phone` int unsigned NOT NULL,
-  `address` varchar(250) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE rireki (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  author_id INTEGER NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  dir_path varchar(1024) NOT NULL,
+  tex varchar(1024) NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES user (id)
+);
+
+INSERT INTO user (username, password)
+VALUES ('oda', 'oda');
+
+INSERT INTO rireki (dir_path, tex, author_id)
+VALUES ('/app/files/0/', 'platex', 1);
--- web_app/flask_app/compiler.py
+++ web_app/flask_app/compiler.py
@@ -43,12 +43,40 @@ def upload():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(UPLOAD_FOLDER, filename))
+
+            # 拡張子を消したファイル名を取得する
             name = filename.rsplit('.', 1)[0]
-            command = "platex {} && dvipdfmx {}".format(name, name)
+
+            # 作業用ディレクトリを作成
+            # 作業用ディレクトリにファイルをコピー
+            # 作業用ディレクトリに移動
+            # コンパイルの実施
+            connection = get_db()
+            cursor = connection.cursor(dictionary=True)
+            #id_dir = int(cursor.lastrowid)
+            cursor.execute("SELECT COUNT(*) FROM rireki WHERE author_id = {}".format(g.user['id']))
+            count = cursor.fetchone()['COUNT(*)']
+            dir_number = hash((g.user['id'], count))
+            dire = UPLOAD_FOLDER + str(dir_number) + "/"
+
+            command = "mkdir -p {0} && cp {2}{1}.tex {0} && cd {0} && platex {1} && dvipdfmx {1}".format(dire, name, UPLOAD_FOLDER)
             subprocess.run(command, shell=True)
             #return redirect(url_for('compiler.uploaded_file', filename=filename))
+
+            # 後の処理（ここは後で変えようと思う）
             compiled_name = name + ".pdf"
-            return redirect(url_for('compiler.compiled_file', compiled_name=compiled_name))
+            #return redirect(url_for('compiler.compiled_file', compiled_name=compiled_name))
+
+            cursor.execute(
+                "INSERT INTO rireki (dir_path, tex, author_id)"
+                " VALUES ('{}', '{}', '{}')".format(
+                    dire, "platex", g.user['id']
+                )
+            )
+            connection.commit()
+            cursor.close()
+            #connection.close()
+            return redirect(url_for('blog.index'))
 
     #return render_template('auth/register.html')
     return render_template('compiler/upload.html')
